de867ddea246854fb4876eae66cf449ac2e71aeb
Adam Jackson <ajax@nwnk.net>
2006-04-07 18:07:53 +0000

    Unlibcwrap. Bump server version requirement. Bump to 1.1.0.

--- src/spaceorb.c
+++ src/spaceorb.c
@@ -38,7 +38,6 @@
 #include <misc.h>
 #include <xf86.h>
 #define NEED_XF86_TYPES
-#include <xf86_ansic.h>
 #include <xf86_OSproc.h>
 #include <xf86Xinput.h>
 #include <xisb.h>
@@ -77,7 +76,7 @@ static XF86ModuleVersionInfo VersionRec =
 	MODINFOSTRING1,
 	MODINFOSTRING2,
 	XORG_VERSION_CURRENT,
-	1, 0, 0,
+	1, 1, 0,
 	ABI_CLASS_XINPUT,
 	ABI_XINPUT_VERSION,
 	MOD_CLASS_XINPUT,
